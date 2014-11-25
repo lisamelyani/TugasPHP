d0f2fbe47e8b9ef6565e59b1eec09f8dfd250fd5
dhanangbilang <dhanilm1@gmail.com>
2014-11-25 20:27:29 +0700

    coba-coba header

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+	
+	</head>
+	<body style="margin:0px;">
+		<div><!-- outer -->
+			<div style="background:#99ff33; width:100%; height:150px;">
+				<table border="1">
+					<tr>
+						<td colspan="3" style="width:1260px;">
+							logo
+						</td>
+						<td style="width:50px;">
+							t
+						</td>
+						<td style="width:50px;">
+							f
+						</td>
+					</tr>
+					<tr style="float: right;">
+						<td style="width: 500px;">
+							Student Activities
+						</td>
+						<td style="width: 500px;">
+							 Activities
+						</td>
+						<td style="width: 500px;">
+							Kalbisiana
+						</td>
+						
+					</tr>
+				</table>
+			</div>
+			</div>
+		</div>
+	</body>
+</html>
