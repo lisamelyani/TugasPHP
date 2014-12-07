45a1084fa6a22aafb03ec834f47d81156fe1a1aa
lisamelyani <lisamelyani@gmail.com>
2014-12-07 14:51:06 +0700

    sampai 109 part 1

--- Widget_Corp/public/create_subject.php
+++ Widget_Corp/public/create_subject.php
@@ -1,4 +1,4 @@
-<?php session_start();?>
+<?php require_once("../includes/session.php");?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 
